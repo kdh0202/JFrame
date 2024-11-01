e4b5556ba524a14736a1e36f1fd8cbafe14eabad
kdh0202 <jellymellow02@gmail.com>
2024-11-01 15:30:30 +0900

    계산기 토대만들기

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -7,24 +7,78 @@ public class CalculatorTest extends JFrame {
     String operator;
     CalculatorTest() {
         setTitle("계산기");
+
         setLayout(new BorderLayout(10,10));
+        showNorth(); showCenter(); showSouth();
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(450, 500);
         setVisible(true);
+    }
 
-        var textField = new JTextField();
+    void showNorth() {
+    JTextField textField = new JTextField();
 
-        add(textField, BorderLayout.NORTH);
+    add (textField, BorderLayout.NORTH);
+    }
 
-        var panel = new JPanel(new GridLayout(4,4));
 
+
+    void showCenter() {
+        JPanel panel = new JPanel();
+        setLayout(new GridLayout(4,4));
         String[] buttons = { "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "*", "/", "0", "Clear", "=" };
 
+        add(panel, BorderLayout.CENTER);
+
+        for(String button : buttons) {
+
+            var btn = new JButton(button);
+            btn.setFont(new Font("Arial", Font.PLAIN, 30));
+            btn.addActionListener(e -> {
+
+                Label textField = null;
+                if (button.charAt(0) >= '0' && button.charAt(0) <= '9') {
+                    textField.setText(textField.getText() + button);
+                }
+                else if (button.equals("Clear")) {
+                    textField.setText("");
+                    operand = "";
+                    operator = "";
+                }
+                else if (button.equals("=")) {
+                    long op1 = Long.valueOf(operand);
+                    long op2 = Long.valueOf(textField.getText());
+
+                    if (operator.equals("+")) {
+                        textField.setText(String.valueOf(op1 + op2));
+                    } else if (operator.equals("-")) {
+                        textField.setText(String.valueOf(op1 - op2));
+                    } else if (operator.equals("*")) {
+                        textField.setText(String.valueOf(op1 * op2));
+                    } else if (operator.equals("/")) {
+                        textField.setText(String.valueOf(op1 / op2));
+                    }
+                }
+                else {
+                    operand = textField.getText();
+                    operator = button;
+                    textField.setText("");
+                }
+            });
+
+            panel.add(btn);
+        }
+
+        add(panel);
     }
 
+    void showSouth() {
+
+    }
     public static void main(String[] args) {
-        new CalculatorTest();
+        var frame = new CalculatorTest();
+
+        frame.setVisible(true);
     }
 }
-
-
