430e8f6fb5f8a61d612528f4c16ef74bc6fd6841
kdh0202 <jellymellow02@gmail.com>
2024-11-01 13:58:57 +0900

    다시만들어보기

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -11,6 +11,15 @@ public class CalculatorTest extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(450, 500);
         setVisible(true);
+
+        var textField = new JTextField();
+
+        add(textField, BorderLayout.NORTH);
+
+        var panel = new JPanel(new GridLayout(4,4));
+
+        String[] buttons = { "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "*", "/", "0", "Clear", "=" };
+
     }
 
     public static void main(String[] args) {
