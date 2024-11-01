4519e3781b96e3b1545627490c1f5e7645d8cc6e
kdh0202 <jellymellow02@gmail.com>
2024-11-01 13:55:43 +0900

    다시시작

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -7,32 +7,10 @@ public class CalculatorTest extends JFrame {
     String operator;
     CalculatorTest() {
         setTitle("계산기");
-
         setLayout(new BorderLayout(10,10));
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(450, 500);
         setVisible(true);
-    }
-    {
-        JPanel panel = new JPanel();
-
-        JTextField area = new JTextField();
-        area.setEditable(false);
-        area.setForeground(Color.BLACK);
-
-        panel.add(area);
-
-        add(panel, BorderLayout.NORTH);
-    }
-
-    {
-        JPanel panel = new JPanel(new GridLayout(4,4));
-
-        String[] buttons = {"7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "*", "/", "0", "Clear", "="};
-
-
-        add(panel, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
